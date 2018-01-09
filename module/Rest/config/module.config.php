cbcdc188e7831bebc77f7a47f55b04fbbb43d3ee
Gedson Marcelino <gedson.marcelino@gmail.com>
2018-01-09 20:26:30 -0300

    - refactor Controller to use the AbstracRestfulController

--- module/Rest/config/module.config.php
+++ module/Rest/config/module.config.php
@@ -12,17 +12,37 @@ return [
             'rest' => [
                 'type'    => Segment::class,
                 'options' => [
-                    'route' => '/rest[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
+                    'route' => '/api[/:id]',
+                    // 'route' => '/api[/:action]',
                     'defaults' => [
-                        'controller' => Controller\RestController::class,
-                        'action'     => 'index',
+                        'controller' => Controller\RestController::class
                     ],
                 ],
             ],
+        ],
+    ],
+
+    // This is default Zend Framework Stuff. We can ignore it.
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+
+        // We need to set this up so that we're allowed to return JSON
+        // responses from our controller.
+        'strategies' => [
+            'ViewJsonStrategy',
         ],
     ],
 
--- module/Rest/src/Controller/RestController.php
+++ module/Rest/src/Controller/RestController.php
@@ -2,23 +2,122 @@
 
 namespace Rest\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
 
-class RestController extends AbstractActionController
+class RestController extends AbstractRestfulController
 {
     
-    public function __construct(){
+    private $users;
+    private $lastId = 3;
 
+    public function __construct(){
+        $this->users = [
+            [ 'id' => 1, 'name' => 'teste', 'age' => 30 ],
+            [ 'id' => 2, 'name' => 'teste 2', 'age' => 21 ],
+            [ 'id' => 3, 'name' => 'teste 3', 'age' => 45 ],
+        ];
     }
 
-    public function indexAction()
+    private function notFound()
     {
-        $obj['name'] = "Name";
-        $obj['age'] = 30;
+        $this->getResponse()->setStatusCode(404);
+        return new JsonModel([
+            'message' => 'Not found'
+        ]);
+    }
 
-        echo json_encode( $obj );
+    public function get( $id ){
+        $user = $this->users[$id];
+        return new JsonModel( $user );
+    }
 
-        die;
+    
+    /**
+     * GET /api
+     * Return list users
+     */
+    public function getList()
+    {
+        return new JsonModel([
+            'users' => $this->users
+        ]);
+    }
+
+
+    /**
+     * POST /api
+     * Create a new user
+     * The $data parameter is automatically mapped from the request body
+     */
+    public function create($data)
+    {
+        $this->lastId = $this->lastId + 1;
+
+        $this->users[] = [
+            'id' => $this->lastId,
+            'name' => $data['name'],
+            'age' => $data['age'],
+        ];
+
+        // Let's return the complete list with the newly created lead
+        return $this->getList();
+    }
+
+    /**
+     * PUT /user/{id}
+     * Updates the user identified by {id}
+     * The $id parameter is automatically mapped from the request URL
+     * and the $data parameter from the request body
+     */    
+    public function update($id, $data)
+    {
+        $userFound = array_filter($this->users, function($u) use ($id) {
+            return $u['id'] == $id;
+        });
+
+        if ($userFound) {
+            $user = array_values($userFound)[0];
+
+            $user['name'] = $data['name'];
+            $user['age'] = $data['age'];
+
+
+            return new JsonModel([
+                'user' => $user 
+            ]);
+        } else {
+            return $this->notFound(); // Return a 404 if the lead is not found
+        }
+    }
+
+    /**
+     * DELETE /user/{id}
+     * Deletes the user identified by {id}
+     * The $id parameter is automatically mapped from the request URL
+     */
+    public function delete($id)
+    {
+        $found = false;
+
+        foreach ($this->users as $index => $user) {
+            if ($user['id'] == $id) {
+                $found = true;
+                break;
+            }
+        }
+
+        if ($found) {
+            array_splice($this->users, $index, 1);
+            return $this->getList();
+        }
+        else {
+            return $this->notFound(); // Return a 404 if the lead is not found
+        }
+    }
+
+    public function myUserAction(){
+        return new JsonModel($this->users[1]);
     }
 
 }
